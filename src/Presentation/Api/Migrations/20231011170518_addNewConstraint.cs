a2d22518e43686eb7f75663fcaae0067336203b4
Meysam <Meysam@MGH>
2023-10-11 20:42:09 +0330

    add Email Constraint

---
+++ src/Presentation/Api/Migrations/20231011170518_addNewConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class addNewConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "core",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                schema: "core",
+                table: "Users");
+        }
+    }
+}
